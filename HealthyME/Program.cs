0154dc83c0e12438ab21bb67db61d80a4b2164b6
Jayesh Shelar <jayesh.shelar@outlook.com>
2022-10-12 21:04:34 +0530

    Intial Commit

--- HealthyME/Program.cs
+++ HealthyME/Program.cs
@@ -1,3 +1,6 @@
+using HealthyME.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace HealthyME
 {
     public class Program
@@ -8,6 +11,10 @@ namespace HealthyME
 
             // Add services to the container.
             builder.Services.AddAuthorization();
+            builder.Services.AddControllers();
+
+            builder.Services.AddDbContext<UserContext>(options=>
+             options.UseSqlServer(builder.Configuration.GetConnectionString("HEM")));
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -24,25 +31,7 @@ namespace HealthyME
 
             app.UseAuthorization();
 
-            var summaries = new[]
-            {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateTime.Now.AddDays(index),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
-
+            app.MapControllers();
             app.Run();
         }
     }
