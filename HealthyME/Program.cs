43b4b999e4cd406006719273f0bb476d060677c8
Jayesh Shelar <jayesh.shelar@outlook.com>
2023-02-02 17:29:32 +0530

    Added ENV Details for Publish

--- HealthyME/Program.cs
+++ HealthyME/Program.cs
@@ -21,19 +21,22 @@ namespace HealthyME
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            
             app.UseHttpsRedirection();
             app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
+            app.UseHttpsRedirection();
             app.UseAuthorization();
-
             app.MapControllers();
             app.Run();
         }
